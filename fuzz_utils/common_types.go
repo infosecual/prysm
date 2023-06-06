da6c6ed51f2af4f1d650fa1caf156f4ce1a8269a
Bhargava Shastry <bhargava.shastry@ethereum.org>
2023-06-06 15:36:07 +0200

    Fuzz response code

--- fuzz_utils/common_types.go
+++ fuzz_utils/common_types.go
@@ -1,14 +1,29 @@
 package fuzz_utils
 
 import (
+	"math/rand"
+
 	"github.com/ethereum/go-ethereum/log"
 )
 
+// https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/p2p-interface.md#responding-side
 var responseCodeSuccess = byte(0x00)
 var responseCodeInvalidRequest = byte(0x01)
 var responseCodeServerError = byte(0x02)
+var responseCodeResourceUnavailable = byte(0x03)
 
 func FuzzRespCode(code *byte) {
 	log.Info("Fuzzing response code")
+	number := rand.Intn(4)
+	switch number {
+	case 0:
+		*code = responseCodeSuccess
+	case 1:
+		*code = responseCodeInvalidRequest
+	case 2:
+		*code = responseCodeServerError
+	case 3:
+		*code = responseCodeResourceUnavailable
+	}
 	return
 }
--- fuzz_utils/errors.go
+++ fuzz_utils/errors.go
@@ -1,10 +1,14 @@
 package fuzz_utils
 
-import "github.com/ethereum/go-ethereum/log"
+import (
+	"github.com/ethereum/go-ethereum/log"
+)
 
 // /////////////////////////////////////////////////////////////////////////////
 // error fuzzers
 // /////////////////////////////////////////////////////////////////////////////
+
+/// Spec citation: https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/p2p-interface.md?plain=1#L585-L601
 
 func FuzzWriteErrorResponseToStream(originalResponseCode byte, originalReason string) (responseCode byte, reason string) {
 	// var responseCodeSuccess = byte(0x00)
@@ -18,5 +22,21 @@ func FuzzWriteErrorResponseToStream(originalResponseCode byte, originalReason st
 func FuzzErrorResponseBytes(OriginalError []byte) []byte {
 	// resp, err := createErrorResponse(responseCode, reason, s.cfg.p2p)
 	log.Info("FUZZ - FuzzErrorResponseBytes")
-	return OriginalError
+	/// Spec
+	/// The response code can have one of the following values, encoded as a single unsigned byte:
+	//	-  0: **Success** -- a normal response follows, with contents matching the expected message schema and encoding specified in the request.
+	//	-  1: **InvalidRequest** -- the contents of the request are semantically invalid, or the payload is malformed, or could not be understood.
+	//	The response payload adheres to the `ErrorMessage` schema (described below).
+	//	-  2: **ServerError** -- the responder encountered an error while processing the request.
+	//	The response payload adheres to the `ErrorMessage` schema (described below).
+	//	-  3: **ResourceUnavailable** -- the responder does not have requested resource.
+	//  The response payload adheres to the `ErrorMessage` schema (described below).
+	//
+	// Clients MAY use response codes above `128` to indicate alternative, erroneous request-specific responses.
+	// The range `[4, 127]` is RESERVED for future usages, and should be treated as error if not recognized expressly.
+	var responseCode byte
+	FuzzRespCode(&responseCode)
+	payload := OriginalError[1:]
+	MutateNBytes(&payload, 255)
+	return append([]byte{responseCode}, payload...)
 }
