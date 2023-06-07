c47645fb86a3ccf39510222a2dd6d3293f4ffb0c
David Theodore <prodigalsonsolutions@gmail.com>
2023-06-07 16:38:18 -0500

    added setup script

---
+++ setup_wormtongue_etb.sh
@@ -0,0 +1,19 @@
+# Download the required repos
+# NOTE this should change back to tyler's repo once he accepts the PR here:
+# https://github.com/0xTylerHolmes/ethereum-testnet-bootstrapper/pull/14
+git clone git@github.com:infosecual/ethereum-testnet-bootstrapper.git
+git clone git@github.com:0xTylerHolmes/etb-fuzzer-images.git
+
+#Build the Wormtongue image
+docker build -t etb-all-clients:latest -f etb-fuzzer-images/wormtongue/deps/dockers/etb-all-clients_mainnet-wormtongue.Dockerfile .
+
+# Move the configs and the launchers into ethereum-testnet-bootstrapper
+cp -r etb-fuzzer-images/wormtongue/deps/* ethereum-testnet-bootstrapper/deps
+cp etb-fuzzer-images/wormtongue/configs/mainnet-current-wormtongue.yaml ethereum-testnet-bootstrapper/configs
+cp etb-fuzzer-images/wormtongue/apps/* ethereum-testnet-bootstrapper/apps
+
+# Prepair ETB for Wormtongue
+cd ethereum-testnet-bootstrapper
+make build-bootstrapper
+make clean config=configs/mainnet-current-wormtongue.yaml
+make init-testnet config=configs/mainnet-current-wormtongue.yaml
