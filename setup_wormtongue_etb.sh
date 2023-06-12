6faaf7a71e5823a6e59e3a913d699a94a823d6cb
David Theodore <prodigalsonsolutions@gmail.com>
2023-06-12 17:56:16 +0200

    changed dockerfile target name in build script

--- setup_wormtongue_etb.sh
+++ setup_wormtongue_etb.sh
@@ -5,7 +5,7 @@ git clone git@github.com:infosecual/ethereum-testnet-bootstrapper.git
 git clone git@github.com:infosecual/etb-fuzzer-images.git
 
 # Build the GEneric client and Wormtongue images
-docker build -t etb-all-clients:latest -f etb-fuzzer-images/wormtongue/deps/dockers/etb-all-clients_generic_no-peer-scoring .
+docker build -t etb-all-clients:latest -f etb-fuzzer-images/wormtongue/deps/dockers/etb-all-clients_generic_no-peer-scoring.Dockerfile .
 docker build -t etb-wormtongue:latest -f etb-fuzzer-images/wormtongue/deps/dockers/wormtongue.Dockerfile .
 
 # Move the configs and the launchers into ethereum-testnet-bootstrapper
