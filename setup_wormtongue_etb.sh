eedda439f164c55313a5fa457cb595367b23d53f
David Theodore <prodigalsonsolutions@gmail.com>
2023-07-10 15:06:27 -0500

    changed launcher script to use new ETB repo

--- setup_wormtongue_etb.sh
+++ setup_wormtongue_etb.sh
@@ -31,22 +31,22 @@ fi
 # NOTE this should change back to tyler's repo once he accepts the PR here:
 # https://github.com/0xTylerHolmes/ethereum-testnet-bootstrapper/pull/14
 git clone git@github.com:infosecual/ethereum-testnet-bootstrapper.git
-git clone git@github.com:infosecual/etb-fuzzer-images.git
+#git clone git@github.com:infosecual/etb-fuzzer-images.git
 
 # Build the Generic client and Wormtongue images
-docker build -t etb-all-clients:latest -f etb-fuzzer-images/wormtongue/deps/dockers/etb-all-clients_generic_no-peer-scoring.Dockerfile .
-docker build -t etb-wormtongue:latest -f etb-fuzzer-images/wormtongue/deps/dockers/wormtongue.Dockerfile .
+#docker build -t etb-all-clients:latest -f etb-fuzzer-images/wormtongue/deps/dockers/etb-all-clients_generic_no-peer-scoring.Dockerfile .
+#docker build -t etb-wormtongue:latest -f etb-fuzzer-images/wormtongue/deps/dockers/wormtongue.Dockerfile .
 
 # Move the configs and the launchers into ethereum-testnet-bootstrapper
-cp -r etb-fuzzer-images/wormtongue/deps/* ethereum-testnet-bootstrapper/deps
-cp etb-fuzzer-images/wormtongue/configs/mainnet-current-wormtongue-$1.yaml ethereum-testnet-bootstrapper/configs
-cp etb-fuzzer-images/wormtongue/apps/* ethereum-testnet-bootstrapper/apps
+#cp -r etb-fuzzer-images/wormtongue/deps/* ethereum-testnet-bootstrapper/deps
+#cp etb-fuzzer-images/wormtongue/configs/mainnet-current-wormtongue-$1.yaml ethereum-testnet-bootstrapper/configs
+#cp etb-fuzzer-images/wormtongue/apps/* ethereum-testnet-bootstrapper/apps
 
 # Prepair ETB for Wormtongue
 cd ethereum-testnet-bootstrapper
-make build-bootstrapper
-make clean config=configs/mainnet-current-wormtongue-$1.yaml
-make init-testnet config=configs/mainnet-current-wormtongue-$1.yaml
+make build-all-images
+make clean
+make init-testnet config=configs/wormtongue-$1.yaml
 
 # print instuctions to run
 echo "#########################################################################"
@@ -59,5 +59,5 @@ echo "#########################################################################"
 echo "# To attach to the health checker:"
 echo "#########################################################################"
 echo ""
-echo "docker logs status-check-0 -f"
+echo "docker logs -f node-watch-0"
 echo ""
